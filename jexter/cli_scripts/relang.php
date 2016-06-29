aac64824362f9e5ead04ecb527785a8f3ebd8dc7
Konstantin Kutcevalov <mail@art-prog.ru>
2016-06-30 00:47:50 +0600

    nothing...

--- jexter/cli_scripts/relang.php
+++ jexter/cli_scripts/relang.php
@@ -4,22 +4,22 @@
  */
 
 $root = __DIR__ . '/../../';
-out("admin/en-GB.com_mycityselector.ini\n", "green");
+out("   admin/en-GB.com_mycityselector.ini\n", "cyan");
 @copy($root.'/administrator/components/com_mycityselector/language/en-GB/en-GB.com_mycityselector.ini', $root.'/administrator/language/en-GB/en-GB.com_mycityselector.ini');
-out("admin/en-GB.com_mycityselector.sys.ini\n", "green");
+out("   admin/en-GB.com_mycityselector.sys.ini\n", "cyan");
 @copy($root.'/administrator/components/com_mycityselector/language/en-GB/en-GB.com_mycityselector.sys.ini', $root.'/administrator/language/en-GB/en-GB.com_mycityselector.sys.ini');
 
-out("admin/ru-RU.com_mycityselector.ini\n", "green");
+out("   admin/ru-RU.com_mycityselector.ini\n", "cyan");
 @copy($root.'/administrator/components/com_mycityselector/language/ru-RU/ru-RU.com_mycityselector.ini', $root.'/administrator/language/ru-RU/ru-RU.com_mycityselector.ini');
-out("admin/ru-RU.com_mycityselector.sys.ini\n", "green");
+out("   admin/ru-RU.com_mycityselector.sys.ini\n", "cyan");
 @copy($root.'/administrator/components/com_mycityselector/language/ru-RU/ru-RU.com_mycityselector.sys.ini', $root.'/administrator/language/ru-RU/ru-RU.com_mycityselector.sys.ini');
 
-out("site/en-GB.com_mycityselector.sys.ini\n", "green");
+out("   site/en-GB.com_mycityselector.sys.ini\n", "cyan");
 @copy($root.'/components/com_mycityselector/language/en-GB/en-GB.com_mycityselector.sys.ini', $root.'/language/en-GB/en-GB.com_mycityselector.sys.ini');
-out("site/en-GB.com_mycityselector.sys.ini\n", "green");
+out("   site/en-GB.com_mycityselector.sys.ini\n", "cyan");
 @copy($root.'/components/com_mycityselector/language/en-GB/en-GB.com_mycityselector.sys.ini', $root.'/language/en-GB/en-GB.com_mycityselector.sys.ini');
 
-out("site/ru-RU.com_mycityselector.ini\n", "green");
+out("   site/ru-RU.com_mycityselector.ini\n", "cyan");
 @copy($root.'/components/com_mycityselector/language/ru-RU/ru-RU.com_mycityselector.ini', $root.'/language/ru-RU/ru-RU.com_mycityselector.ini');
-out("site/ru-RU.com_mycityselector.sys.ini\n", "green");
+out("   site/ru-RU.com_mycityselector.sys.ini\n", "cyan");
 @copy($root.'/components/com_mycityselector/language/ru-RU/ru-RU.com_mycityselector.sys.ini', $root.'/language/ru-RU/ru-RU.com_mycityselector.sys.ini');
