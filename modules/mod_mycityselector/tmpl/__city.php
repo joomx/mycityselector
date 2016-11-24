c4150b7d2c96b1fd360847256a2c7e3236688cbd
Konstantin <adamasantares@gmail.com>
2016-11-24 09:32:56 +0600

    fix https://github.com/art-programming-team/mycityselector/issues/19

--- modules/mod_mycityselector/tmpl/__city.php
+++ modules/mod_mycityselector/tmpl/__city.php
@@ -4,10 +4,12 @@ defined('_JEXEC') or exit(header("HTTP/1.0 404 Not Found") . '404 Not Found');
  * City item template
  *
  * @var $this MyCitySelectorModule
- * @var $cities array
- *          ['субдомен' => 'название']
+ * @var $cities array ['субдомен' => 'название']
  * @var $province string
+ * @var $baseDomain string
+ * @var $returnUrl string
  * @var $city string
+ * @var $defaultCityCode string
  */
 ?>
 <div class="cities<?= isset($cities[$city]) ? ' active' : ' hidden' ?> group-<?= $province ?>">
@@ -16,7 +18,11 @@ defined('_JEXEC') or exit(header("HTTP/1.0 404 Not Found") . '404 Not Found');
     foreach ($cities as $cityKey => $data) {
         $url = $returnUrl;
         if ($onSubdomains == '1') {
-            $url = preg_replace('#^(http|https)(://)([^\/]*)(.*)$#', '$1$2' . $cityKey . '.' . $baseDomain . '$4', $returnUrl);
+            if ($defaultCityCode == $cityKey) { // if this city is city of base domain
+                $url = preg_replace('#^(http|https)(://)([^\/]*)(.*)$#i', '$1$2' . $baseDomain . '$4', $returnUrl);
+            } else {
+                $url = preg_replace('#^(http|https)(://)([^\/]*)(.*)$#i', '$1$2' . $cityKey . '.' . $baseDomain . '$4', $returnUrl);
+            }
         }
         ?>
         <div class="city">
